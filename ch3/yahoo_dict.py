d47717e9914638d3f1a3a88b704934b8ae9c4e2e
Darrell <darrell@novellink.com>
2018-05-12 02:13:11 +0800

    tutorial : fix yahoo dictionary with new structure

--- ch3/yahoo_dict.py
+++ ch3/yahoo_dict.py
@@ -1,15 +1,16 @@
+import sys
+
 import requests
 from bs4 import BeautifulSoup
 import urllib.parse
 
-
-Y_DICT_URL= 'https://tw.dictionary.yahoo.com/dictionary?p='
+Y_DICT_URL = 'https://tw.dictionary.yahoo.com/dictionary?p='
 
 
 def get_web_page(url, query):
     query = urllib.parse.quote_plus(query)
     resp = requests.get(
-        url+query,
+        url + query,
         headers={'Referer': 'https://tw.dictionary.yahoo.com/dictionary?'}
     )
     if resp.status_code != 200:
@@ -21,12 +22,18 @@ def get_web_page(url, query):
 
 def get_dict_info(dom):
     soup = BeautifulSoup(dom, 'html5lib')
-    for li in soup.find('ul', 'explanations').find_all('li', 'exp-item'):
-        print(li.text)
+    for li in soup.find('div', 'p-rel').find_all('li'):
+        grammar = li.find_all('div')[0].text
+        if len(li.find_all('div')) > 1:
+            note = li.find_all('div')[1].text
+        else:
+            note = None
+
+        print(grammar, note)
 
 
 if __name__ == '__main__':
-    #page = get_web_page(Y_DICT_URL, '非常')
-    page = get_web_page(Y_DICT_URL, 'out of order')
+    page = get_web_page(Y_DICT_URL, '非常')
+    # page = get_web_page(Y_DICT_URL, 'QUERY')
     if page:
         get_dict_info(page)
